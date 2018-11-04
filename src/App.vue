7407617a23daf85bc56143ec4ef071ba944d6bed
mickey_ichi <lemanhhungb01501@gmail.com>
2018-11-04 22:44:33 +0700

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <TodoList v-bind:list="todos" @onItemClick="onTodoClick"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import TodoList from './components/TodoList.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            TodoList,
+        },
+        data: function () {
+            return {
+                todos: [{name: 'mickey'}],
+            }
+        },
+        mounted: function () {
+            setTimeout(() => {
+                this.todos.push({
+                    name: 'mickey',
+                })
+            }, 2000);
+        },
+        methods: {
+            onTodoClick: function (index, item) {
+                this.$set(this.todos, index, {
+                    name: `${item.name} hehe`
+                });
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,32 @@
 <template>
-    $END$
+    <div>
+        <div class="mickey" :key="index"
+             v-for="(item, index) in list"
+             @click="onItemClick(index, item)">
+            {{item.name}}
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
-        name: "TodoList"
+        name: "TodoList",
+        props: {
+            list: {
+                type: Array,
+                required: true,
+            },
+        },
+        methods: {
+            onItemClick: function (index, item) {
+                this.$emit('onItemClick',index, item);
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .mickey {
+        cursor: pointer;
+    }
 </style>
